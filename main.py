2c23a598a2c4dea333d4f0f221938fb28b3ddfd4
Sam Tholath Robin <samtholathwrk@gmail.com>
2023-11-08 12:52:00 +0530

    Edited main.py and added classes

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from ultralytics import YOLO
 import cv2
-import math 
-
 
 # model
 model = YOLO("yolo-Weights/yolov8n.pt")
+names=model.names
+p_id = list(names)[list(names.values()).index('person')]
+
 # object classes
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
@@ -18,8 +19,10 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "teddy bear", "hair drier", "toothbrush"
               ]
 
+num_person=''
+
 # initialize the webcam
-cap = cv2.VideoCapture(0) 
+cap = cv2.VideoCapture('pexels_videos_4698 (1080p).mp4') 
 cap.set(3, 640)
 cap.set(4, 480)
 
@@ -28,24 +31,21 @@ while True:
     results = model(img, stream=True)
     # coordinates
     for r in results:
+        num_person="Number of people : "+str(r.boxes.cls.tolist().count(p_id))
+        cv2.putText(img,num_person,[20,35],cv2.FONT_HERSHEY_SIMPLEX,1.5,(0, 0, 255),3)
         boxes = r.boxes
         for box in boxes:
             # bounding box
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
             # put box in cam
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-            # confidence
-            confidence = math.ceil((box.conf[0]*100))/100
-            print("Confidence --->",confidence)
-            # class name
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255,230,0), 3)
+            #idx of class name in classNames
             cls = int(box.cls[0])
-            print("Class name -->", classNames[cls])
-            # object details
             org = [x1, y1]
             font = cv2.FONT_HERSHEY_SIMPLEX
             fontScale = 1
-            color = (255, 0, 0)
+            color = (255,0,0)
             thickness = 2
             cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
 
